63d9bb1e7207bac5fbf23796c5ebf24878ade42a
web100ry <valerij.ivanjuk@gmail.com>
2018-05-11 23:51:31 +0300

    Lessons 7

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -19,4 +19,11 @@ class CategoryController extends AppController
        return $this->render('index', compact('hits'));
 
     }
+    public function actionView($id){
+    $id=Yii::$app->request->get('id');
+    //debug($id);
+        $products=Product::find()->where(['category_id'=> $id])->all();
+ //    debug($products);
+     return $this->render('view', compact('products') );
+    }
 }
