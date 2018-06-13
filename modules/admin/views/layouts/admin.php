774772ab1ad4c24142e62902c1a985838084f83e
web100ry <valerij.ivanjuk@gmail.com>
2018-06-14 00:08:40 +0300

    Lessons 26_1

--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -133,24 +133,23 @@ AppLtAsset::register($this);
                         </div>
                         <div class="mainmenu pull-left">
                             <ul class="nav navbar-nav collapse navbar-collapse">
-                                <li><a href="index.html" class="active">Home</a></li>
-                                <li class="dropdown"><a href="#">Shop<i class="fa fa-angle-down"></i></a>
+                                <li><a href="<?= \yii\helpers\Url::to(['/admin'])?>" class="active">Головна</a></li>
+                                <li class="dropdown"><a href="#">Категорії<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
-                                        <li><a href="shop.html">Products</a></li>
-                                        <li><a href="product-details.html">Product Details</a></li>
-                                        <li><a href="checkout.html">Checkout</a></li>
-                                        <li><a href="cart.html">Cart</a></li>
-                                        <li><a href="login.html">Login</a></li>
+                                        <li><a href="<?= \yii\helpers\Url::to(['category/index'])?>">Список категорій</a></li>
+                                        <li><a href="<?= \yii\helpers\Url::to(['category/create'])?>">Додати категорію</a></li>
                                     </ul>
                                 </li>
-                                <li class="dropdown"><a href="#">Blog<i class="fa fa-angle-down"></i></a>
+                                <li class="dropdown"><a href="#">Товари<i class="fa fa-angle-down"></i></a>
                                     <ul role="menu" class="sub-menu">
-                                        <li><a href="blog.html">Blog List</a></li>
-                                        <li><a href="blog-single.html">Blog Single</a></li>
+                                        <li><a href="<?= \yii\helpers\Url::to(['category/index'])?>">Список товарів</a></li>
+                                        <li><a href="<?= \yii\helpers\Url::to(['category/create'])?>">Додати товар</a></li>
                                     </ul>
                                 </li>
-                                <li><a href="404.html">404</a></li>
-                                <li><a href="contact-us.html">Contact</a></li>
+                               <!--
+                               <li><a href="404.html">404</a></li>
+                               <li><a href="contact-us.html">Contact</a></li>
+                                -->
                             </ul>
                         </div>
                     </div>
