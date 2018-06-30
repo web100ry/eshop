639dd06874917d7ff95e7e5035c2ebc3c794279e
web100ry <valerij.ivanjuk@gmail.com>
2018-06-30 22:06:47 +0300

    Work with images 1

--- modules/admin/views/product/index.php
+++ modules/admin/views/product/index.php
@@ -6,7 +6,7 @@ use yii\grid\GridView;
 /* @var $this yii\web\View */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Products';
+$this->title = 'Перелік товарів';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="product-index">
@@ -14,7 +14,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Create Product', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Додати товар', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?= GridView::widget([
